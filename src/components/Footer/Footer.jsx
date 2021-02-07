aefb17da366dd95d849e3ddf2ea6c787d5566332
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2021-02-06 23:06:56 -0500

    Testing Footer issue

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,7 @@ const Footer = () => {
     <div className="footer-container">
       <footer className="footer">
         <div className="container">
-          <span className="text-muted">
           <i className="far fa-copyright fa-lg"> Copyright by Harsha Saddanathan 2021</i>
-          </span>
         </div>
       </footer>
     </div>
